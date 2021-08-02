408dfa500d385ddaa35033149e3b4ffbd9cfb675
heychazza <ch@rli.es>
2021-08-02 16:54:01 +0100

    Added code

---
+++ app.js
@@ -0,0 +1,124 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const check = require('./check')
+
+async function loadCodes() {
+    let contents = fs.readFileSync('./codes.txt', { encoding: 'utf-8'})
+    return contents.trim().split('\n');
+}
+
+async function openBrowser() {
+    const browser = await puppeteer.launch({ headless: false });
+    return browser;
+}
+
+async function checkCode(code, browser) {
+    const page = await browser.newPage();
+
+    await page.setViewport({ width: 0, height: 0});
+    await page.goto("https://social.beefeater.co.uk/");
+    {
+      const targetPage = page;
+      const frame = targetPage.mainFrame();
+      const element = await frame.waitForSelector("aria/Enter code");
+      await element.click();
+    }
+    {
+      const targetPage = page;
+      const frame = targetPage.mainFrame();
+      const element = await frame.waitForSelector("aria/Enter code");
+      await element.type(code);
+    }
+    {
+      const targetPage = page;
+      const frame = targetPage.mainFrame();
+      const element = await frame.waitForSelector("aria/CONTINUE");
+      await element.click();
+    }
+    {
+      const targetPage = page;
+      const frame = targetPage.mainFrame();
+      const element = await frame.waitForSelector("aria/Email Address");
+      await element.click();
+    }
+    {
+      const targetPage = page;
+      const frame = targetPage.mainFrame();
+      const element = await frame.waitForSelector("aria/Email Address");
+      await element.type("charliejosephxp+" + code + "@gmail.com");
+    }
+    {
+      const targetPage = page;
+      const frame = targetPage.mainFrame();
+      const element = await frame.waitForSelector("aria/First Name");
+      await element.type("charlie");
+    }
+    {
+      const targetPage = page;
+      const frame = targetPage.mainFrame();
+      const element = await frame.waitForSelector("aria/Last Name");
+      await element.type("joseph");
+    }
+    {
+      const targetPage = page;
+      const frame = targetPage.mainFrame();
+      const element = await frame.waitForSelector("aria/I agree to the terms & conditions");
+      await element.click();
+    }
+    {
+      const targetPage = page;
+      const frame = targetPage.mainFrame();
+      const element = await frame.waitForSelector("aria/I agree to the terms & conditions");
+      await element.type("on");
+    }
+    await sleep(500)
+    {
+      const targetPage = page;
+      const frame = targetPage.mainFrame();
+      const element = await frame.waitForSelector("aria/LET’S PLAY!");
+      await element.click();
+    }
+    await sleep(250)
+    try {
+        {
+          const targetPage = page;
+          const frame = targetPage.mainFrame();
+          const element = await frame.waitForSelector("aria/SPIN THE WHEEL");
+          await element.click();
+        }
+    } catch {
+        console.log(`[${code}] Failed to check code.`)
+    }
+
+    await page.close()
+}
+
+async function run() {
+    let codes = await loadCodes()
+    console.log(`[BeefCrack] Loaded ${codes.length} codes!`)
+
+    let browser = await openBrowser()
+    console.log(`[BeefCrack] Opened browser.`)
+
+    for(i = 0; i < codes.length; i++) {
+        let code = codes[i]
+        console.log(`[BeefCrack] Checking '${code}'.`)
+
+        if(await check.checkCode(code)) {
+            console.log(`[BeefCrack] Code '${code}' is valid! Entering code..`)
+            await checkCode(code, browser)
+        } else {
+            console.log(`[BeefCrack] Code '${code}' is invalid! Skipping code..`)
+        }
+
+        await sleep(2000)
+    }
+}
+run();
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+// openBrowser();
---
+++ check.js
@@ -0,0 +1,42 @@
+var request = require('request');
+var fs = require('fs');
+
+var headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:90.0) Gecko/20100101 Firefox/90.0',
+    'Accept': '*/*',
+    'Accept-Language': 'en-GB,en;q=0.5',
+    'Referer': 'https://social.beefeater.co.uk/',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Type': 'multipart/form-data; boundary=---------------------------6771139122198822583988785286',
+    'Origin': 'https://social.beefeater.co.uk',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'TE': 'trailers',
+};
+
+
+async function getCode(code) {
+    return new Promise(function (resolve, reject) {
+        request({
+            url: 'https://social.beefeater.co.uk/api/entrant/code',
+            method: 'POST',
+            headers: headers,
+            body: `'$-----------------------------6771139122198822583988785286\r\nContent-Disposition: form-data; name="brand"\r\n\r\nbeefeater\r\n-----------------------------6771139122198822583988785286\r\nContent-Disposition: form-data; name="voucher_code"\r\n\r\n${code}\r\n-----------------------------6771139122198822583988785286--\r\n'`,
+            // json: true
+        }, function (error, response, body) {
+            resolve(JSON.parse(body))
+        });
+    })
+}
+
+
+async function checkCode(code) {
+    let body = await getCode(code)
+
+    return body.next == 'data-capture'
+}
+
+
+module.exports.checkCode = checkCode
